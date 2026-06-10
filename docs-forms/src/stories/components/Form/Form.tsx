fe3249de4dbb4d966b838bfcbc03466c022ea359
gonzalo <gonzalososa342@gmail.com>
2026-06-10 00:39:27 -0300

    Add states

--- docs-forms/src/stories/components/Form/Form.tsx
+++ docs-forms/src/stories/components/Form/Form.tsx
@@ -7,25 +7,44 @@ import { useState } from 'react';
 const ComponentTypes = ['text', 'date', 'radio', 'select', 'checkbox', 'rating', 'likert']
 
 const Form: React.FC<FormProps> = ({ fields }) => {
-    const items = fields ? mapToDxItems(fields) : [];
+    const [items, setItems] = useState(() => fields ? mapToDxItems(fields) : []);
     const [newComponentVisible, setNewComponentVisible] = useState<boolean>(false);
+    const [componentEditorVisible, setComponentEditorVisible] = useState<boolean>(false);
+
+    const handleAddComponent = (type: string) => {
+        setNewComponentVisible(false);
+        setComponentEditorVisible(true);
+    }
 
     return (
-        <>
-            <DxForm
-                formData={{}}
-                items={items}
-            // colCount={2}
-            />
-            <Button type="primary" icon={newComponentVisible ? 'minus' : 'plus'} text='Agregar componente' stylingMode='text' onClick={(_e) => setNewComponentVisible(!newComponentVisible)} style={{ marginTop: '2rem' }} />;
-            {newComponentVisible && <>
-                <div style={{ width: "100%", display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', gridTemplateRows: 'auto', gap: '8px', marginTop: '1rem' }}>
-                    {ComponentTypes.map(type => (
-                        <Button text={type} onClick={(_e) => { }} />
-                    ))}
-                </div>
-            </>}
-        </>
+        <div
+            onClick={() => {
+                setNewComponentVisible(false);
+                setComponentEditorVisible(false);
+            }}
+        >
+            <div onClick={(e) => e.stopPropagation()}>
+                <DxForm
+                    formData={{}}
+                    items={items}
+                // colCount={2}
+                />
+                {
+                    !componentEditorVisible &&
+                    <Button type="primary" icon={newComponentVisible ? 'minus' : 'plus'} text='Agregar componente' stylingMode='text' onClick={(_e) => setNewComponentVisible(!newComponentVisible)} style={{ marginTop: '2rem' }} />
+                }
+                {
+                    newComponentVisible && <>
+                        <div style={{ width: "100%", display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', gridTemplateRows: 'auto', gap: '8px', marginTop: '1rem' }}>
+                            {ComponentTypes.map(type => (
+                                <Button text={type} onClick={(_e) => handleAddComponent(type)} />
+                            ))}
+                        </div>
+                    </>
+                }
+                {componentEditorVisible && <div style={{ marginTop: '1rem' }}>Editor</div>}
+            </div>
+        </div>
     );
 };
 
